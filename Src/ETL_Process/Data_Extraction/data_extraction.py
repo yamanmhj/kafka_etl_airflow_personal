1ddaf6c3522f54f12cfe26575b9ae1320ab2c00e
yamanmhj <yamanmhjr@gmail.com>
2024-09-22 12:45:17 +1000

    new modification

--- Src/ETL_Process/Data_Extraction/data_extraction.py
+++ Src/ETL_Process/Data_Extraction/data_extraction.py
@@ -1,26 +1,27 @@
 import os
 from os.path import dirname, abspath
 import pandas as pd
-from  components.get_config_file import get_config_file_data_from_path
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(abspath(__file__))), 'src'))
 
+from Components.get_config_file import get_config_Full_file
 
-class ExtractDataandconfigyaml:
-    def __init__(self, consumer_data_directory):
-        self.consumer_data_directory = consumer_data_directory
-
-
+class ExtractData:
+    def __init__(self):
+        pass
 
     def extract_csv_from_folder(self):
         data_frames = []
-        csv_Path = os.path.join(os.getcwd(),'LoadCustomerData')
-        for file in os.listdir(csv_Path):
+        config_full_file = get_config_Full_file()
+        csv_Path_Location = config_full_file['StoreApiDataIn']
+        print("Path Location is", csv_Path_Location)
+        
+        # Loop through the files in the directory
+        for file in os.listdir(csv_Path_Location):
             if file.endswith('.csv'):
-                file_path = os.path.join(csv_Path, file)
-                df = pd.read_csv(file_path)
+                new_file_path = os.path.join(csv_Path_Location, file) 
+                df = pd.read_csv(new_file_path)
                 data_frames.append(df)
-                
+
         return data_frames
 
 
